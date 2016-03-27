abd3839b322390d73ca63a86845bfcdbf22891f7
KunalKarnik <kunalkarnik95@gmail.com>
2016-03-27 09:56:13 -0500

    Test
    Testing commits

--- breakdown.hs
+++ breakdown.hs
@@ -1,5 +1,7 @@
 getList = filter isRegularFileOrDirectory <$> getDirectoryContents "./data/"
 
+test :: Int -> Int
+test x = 5
 
 doall :: IO [FilePath] -> IO [String]
 doall x = if (fmap (== 0) (fmap length x)) then return else (doit 0)
