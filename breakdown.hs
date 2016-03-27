791fb7945ec6a4aaea06d30386aad42d526f6b5f
c732r120 <chinmay.ratnaparkhi@gmail.com>
2016-03-27 15:23:27 -0500

    InvInd, Stat, Functions to addPostings
    Creates an Inverted Index with word postings & their stats.

--- breakdown.hs
+++ breakdown.hs
@@ -18,3 +18,11 @@ nebula x = (space x) >>= hGetContents
 
 space :: FilePath -> IO Handle
 space x = openFile x ReadMode
+
+
+-- OLD ADD
+buildInd :: String -> Int -> InvInd -> InvInd
+buildInd x y EndInd = (Entry x (Full y 1 EndStat) EndInd)
+buildInd x y z		= if (findPostings x z) == EndStat 
+					  then (Entry x (Full y 1 EndStat) z) 
+					  else (replacePosting x y z)
