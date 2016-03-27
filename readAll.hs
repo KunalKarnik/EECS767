14a8c4d09f1daaf0340e540e4da48479a303dd31
c732r120 <chinmay.ratnaparkhi@gmail.com>
2016-03-27 15:38:03 -0500

    Experimental Code
    Something.hs contains InvInd & Stat data structures

--- readAll.hs
+++ readAll.hs
@@ -51,3 +51,30 @@ correctPath x = fmap ("data/" ++ ) x
 -- Gives you a particular element from getList
 getElem :: Int -> IO [Char]
 getElem m = correctPath $ fmap (!! m) getList
+
+------ TEST FUNCTIONS ------
+data Stat = EndStat | Full Int Int Stat deriving (Show, Eq)
+data InvInd = EndInd | Entry String Stat InvInd deriving (Show)
+
+addDoc ::  InvInd -> [String] -> InvInd
+addDoc x [] = x
+addDoc  z (x:xs)= addDoc (addPosting 1 z x) xs 
+
+
+addPosting :: Int -> InvInd -> String -> InvInd
+addPosting y EndInd x = (Entry x (Full y 1 EndStat) EndInd)
+addPosting y (Entry inDicString (Full docID docFreq rest) restInd) x = 
+					if x == inDicString 
+					then (Entry inDicString (Full docID (docFreq+1) rest) restInd)
+					else (Entry inDicString (Full docID (docFreq) rest) (addPosting y restInd x ))
+
+
+
+findPostings :: String -> InvInd -> Stat
+findPostings [] _ = EndStat
+findPostings _ EndInd = EndStat
+findPostings x (Entry a b c) = if x == a then b else (findPostings x c)
+
+getDF :: Stat -> Int -> Int
+getDF EndStat x = x
+getDF (Full a b c) x = (getDF c (x+1))
